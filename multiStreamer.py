c25e71785016bd5ff5b0381e4610c0c0e22cd007
thjread <thjread@gmail.com>
2018-01-21 10:58:46 +0000

    Clean up

--- multiStreamer.py
+++ multiStreamer.py
@@ -2,8 +2,8 @@
 import multiprocessing, time, sys, datetime
 import sentiment
 import redditStreamer
-import twitterStreamer # TODO
-import priceStreamer # TODO
+import twitterStreamer
+import priceStreamer
 
 # read arguments to choose currency, default to BTC
 currency = "bitcoin"
@@ -16,22 +16,20 @@ redditQueue = multiprocessing.Queue()
 redditProcess = multiprocessing.Process(target=redditStreamer.stream_reddit_comments, args=(redditQueue,currency,))
 redditProcess.start()
 
-# get the twitter streamer TODO
+# get the twitter streamer
 twitterQueue = multiprocessing.Queue()
 twitterProcess = multiprocessing.Process(target=twitterStreamer.stream_tweets, args=(twitterQueue,currency,))
 twitterProcess.start()
 
-# get the price streamer TODO
+# get the price streamer
 priceQueue = multiprocessing.Queue()
 priceProcess = multiprocessing.Process(target=priceStreamer.stream_prices, args=(priceQueue,currency,))
 priceProcess.start()
 
 # output to log file!
 timeStampAtStart = str(datetime.datetime.now())[:-7]
-#logFileName = ("logs/"+currency+"Log_"+timeStampAtStart+".csv").replace(":","-")
 logFileName = ("logs/"+currency+"_LOG.csv")
 logFile = open(logFileName,'a')
-#logFile.write(replyLog)
 
 # stream all the things!
 blockNumber = 0
@@ -52,8 +50,6 @@ while True:
         twitterAPIsen = sentiment.APIsen(tweet)
         twitterSens.append(round(twitterAPIsen,3))
         twitterSensALL.append(round(twitterAPIsen,3))
-##    redditAvg = sum(redditSens)/len(redditSens) if len(redditSens) > 0 else 0.5
-##    twitterAvg = sum(twitterSens)/len(twitterSens)if len(twitterSens) > 0 else 0.5
     while not priceQueue.empty():
         prices = priceQueue.get()
 
@@ -62,12 +58,7 @@ while True:
     logFile.write(logEntry)
     logFile.flush()
     print(logEntry[:-1])
-    
-    # output moving average
-##    nSentiments = 10
-##    redditMvgAvg = str(round(sum(redditSensALL[-nSentiments:])/nSentiments,3)) if len(redditSensALL) > nSentiments else "NaN"
-##    twitterMvgAvg = str(round(sum(twitterSensALL[-nSentiments:])/nSentiments,3)) if len(twitterSensALL) > nSentiments else "NaN"
-##    print("REDDIT AVG: "+redditMvgAvg.ljust(10)+"TWITTER AVG: "+twitterMvgAvg.ljust(10))
+
     time.sleep(1)
 
 
